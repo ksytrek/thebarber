04b5fed48b8090f42d6d2e1de12c42c7f087753d
SOMPHOL WILA <sompholwilaza101@gmail.com>
2022-02-25 00:08:37 +0700

    dsf

--- booking.php
+++ booking.php
@@ -52,7 +52,7 @@ if (isset($_POST['submit'])) {
 
         $custid = $_SESSION['id'];
         $services = $_POST['services'] == ' ' ? 'null' : $_POST['services'];
-        $services2 = $_POST['services2'] == ' '  ? 'null' : $_POST['services2'];
+        $services2 = $_POST['services2'] == null  ? 'null' : $_POST['services2'];
         // $techid = $_POST['tech'];
         // $date = $_POST['dates'];
         // $thistime = $_POST['timess'];
